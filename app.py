fd2e5c348856489394887e712c9b509bc8675b6e
banjocat <jackmuratore@gmail.com>
2016-12-12 01:19:44 -0500

    Use an example for flask-ask directly for testing

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
-from flask_ask import Ask, statement
-
 app = Flask(__name__)
-ask = Ask(app, '/')
+ask = Ask(app, "/")
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
-@ask.intent('HelloIntent')
-def hello(firstname):
-    test = render_template('jack')
-    return statement(text).simple_card('jack', text)
+
+@ask.launch
+def launch():
+    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
+    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+
+
+@ask.intent('HelloWorldIntent')
+def hello_world():
+    speech_text = 'Hello world'
+    return statement(speech_text).simple_card('HelloWorld', speech_text)
+
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    speech_text = 'You can say hello to me!'
+    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+
+
+@ask.session_ended
+def session_ended():
+    return "", 200
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
