94d57d051055fa4869353a4afb76dfe11ed51004
banjocat <jackmuratore@gmail.com>
2016-12-13 00:16:33 -0500

    Working alexa!!!

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import logging
+
+from flask import Flask
+from flask_ask import Ask, question, statement
+
+
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
@@ -5,25 +11,23 @@ logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 @ask.launch
 def launch():
-    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    welcome_message = "Hello. Walter is a baby. He is a beautiful baby that we love. Is Walter the best baby?"
+    return question(welcome_message)
 
+@ask.intent('HelloIntent')
+def hello():
+    greeting = 'Hello Walter'
+    return statement(greeting)
 
-@ask.intent('HelloWorldIntent')
-def hello_world():
-    speech_text = 'Hello world'
-    return statement(speech_text).simple_card('HelloWorld', speech_text)
+@ask.intent('NoIntent')
+def no_intent():
+    what = 'No. You are wrong. Why would you say such a thing. Walter is the best baby!'
+    return statement(what)
 
-
-@ask.intent('AMAZON.HelpIntent')
-def help():
-    speech_text = 'You can say hello to me!'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
-
-
-@ask.session_ended
-def session_ended():
-    return "", 200
+@ask.intent('YesIntent')
+def yes_intent():
+    yes = 'Yes. You are correct. Walter is the best baby.'
+    return statement(yes)
 
 
 if __name__ == '__main__':
